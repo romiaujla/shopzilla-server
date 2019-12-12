c07b06aea8b8abe9102123fe816e3b86cab73c1c
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-12-11 20:11:28 -0500

    created migrations for seller

---
+++ migrations/001.undo.create-v-store-db.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS seller_products;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS seller;
+DROP TABLE IF EXISTS users;
+DROP TYPE IF EXISTS service_type_enum;
+DROP TYPE IF EXISTS user_type_enum;
