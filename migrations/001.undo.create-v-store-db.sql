8ab4e93f47ddf18446e63ea66b629d1fbe9c8200
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-12-19 20:56:04 -0500

    created actuall dummy data

--- migrations/001.undo.create-v-store-db.sql
+++ migrations/001.undo.create-v-store-db.sql
@@ -1,8 +1,9 @@
 DROP TABLE IF EXISTS reviews;
 DROP TABLE IF EXISTS buyer;
-DROP TABLE IF EXISTS seller_products;
+DROP TABLE IF EXISTS shop_products;
 DROP TABLE IF EXISTS products;
-DROP TABLE IF EXISTS seller;
+DROP TABLE IF EXISTS shop;
 DROP TABLE IF EXISTS users;
 DROP TYPE IF EXISTS service_type_enum;
 DROP TYPE IF EXISTS user_type_enum;
+DROP TYPE IF EXISTS availability_enum;
