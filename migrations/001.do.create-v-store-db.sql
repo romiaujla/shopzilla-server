b0aa394b588da3b6f42fef79bada4b3f7236999d
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2019-12-21 13:52:16 -0500

    new stores and user

--- migrations/001.do.create-v-store-db.sql
+++ migrations/001.do.create-v-store-db.sql
@@ -29,7 +29,8 @@ CREATE TYPE service_type_enum AS ENUM(
     'home and party decor',
     'educational',
     'body healing',
-    'tattoo and piercing'
+    'tattoo and piercing',
+    'sports and hobbies'
 );
 
 CREATE TABLE shop(
@@ -83,11 +84,16 @@ CREATE TABLE reviews(
 
 INSERT INTO users (username, password, user_type) VALUES 
     ('shopuser01', '$2a$12$fN64sChSm6YICplSHtc70eM8v3auCG6cdzSjZyP5bCLDE9kDsQQPe', 'shop'),
-    ('shopuser02', '$2a$12$fN64sChSm6YICplSHtc70eM8v3auCG6cdzSjZyP5bCLDE9kDsQQPe', 'shop');
+    ('shopuser02', '$2a$12$fN64sChSm6YICplSHtc70eM8v3auCG6cdzSjZyP5bCLDE9kDsQQPe', 'shop'),
+    ('shopuser03', '$2a$12$fN64sChSm6YICplSHtc70eM8v3auCG6cdzSjZyP5bCLDE9kDsQQPe', 'shop'),
+    ('shopuser04', '$2a$12$fN64sChSm6YICplSHtc70eM8v3auCG6cdzSjZyP5bCLDE9kDsQQPe', 'shop'),
+    ('shopuser05', '$2a$12$fN64sChSm6YICplSHtc70eM8v3auCG6cdzSjZyP5bCLDE9kDsQQPe', 'shop'),
+    ('shopuser06', '$2a$12$fN64sChSm6YICplSHtc70eM8v3auCG6cdzSjZyP5bCLDE9kDsQQPe', 'shop');
 
 INSERT INTO shop(login_id, shop_name, address, start_date, end_date, opening_time, closing_time, service_type, description, image_url) VALUES
-    (1, 'Flip Flop USA', '90 Kent Ave, East River State Park, Brooklyn, NY 11211', CURRENT_DATE-5, CURRENT_DATE+2,  '10:00:00', '22:00:00', 'clothing and accessories', 'Since 2004, we’ve been curating quality flip flops and casual footwear for the beach and beyond. Whatever your mood or style, you’ll find the product to match it.', 'flipflops.png'),
-    (2, 'Tullys Coffee', '657C Gates St., Rialto, CA 92376', CURRENT_DATE+1,CURRENT_DATE+3, '12:00:00', '18:00:00', 'food and drinks', 'This "slow and low" ideology is shared by our four handpicked artists, who believe in the craft and care of their work. Follow our journey from San Francisco to Seattle as we meet four artists who add their own special spark to our Tully’s camper.', 'tully.png'  );
+    (1, 'Flipflops USA', '90 Kent Ave, East River State Park, Brooklyn, NY 11211', CURRENT_DATE-5, CURRENT_DATE+2,  '10:00:00', '22:00:00', 'clothing and accessories', 'Since 2004, we’ve been curating quality flip flops and casual footwear for the beach and beyond. Whatever your mood or style, you’ll find the product to match it.', 'flipflops.png'),
+    (2, 'Tullys Coffee', '657C Gates St., Rialto, CA 92376', CURRENT_DATE+1,CURRENT_DATE+3, '12:00:00', '18:00:00', 'food and drinks', 'This "slow and low" ideology is shared by our four handpicked artists, who believe in the craft and care of their work. Follow our journey from San Francisco to Seattle as we meet four artists who add their own special spark to our Tully’s camper.', 'tully.png'  ),
+    (3, 'Custom Boards', '9556 James Dr., Romeoville, IL 60446', CURRENT_DATE+1,CURRENT_DATE+3, '12:00:00', '18:00:00', 'sports and hobbies', 'Custom made skateboards', 'customboards.png'  );
 
 
 -- INSERT INTO buyer(login_id, name) VALUES
