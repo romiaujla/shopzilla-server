0267d98caa8357d647f234c7bfbbbe6ef59bd1a0
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-12-25 11:31:37 -0500

    created the route for GET /api/shops/:id/products

--- src/shop/shop-router.js
+++ src/shop/shop-router.js
@@ -63,11 +63,28 @@ ShopRouter
     .get((req, res, next) => {
         const {id} = req.params;
         const db = req.app.get('db');
+        return ShopService.getProductsByShopId(db, id)
+            .then((products) => {
+                if(!products){
+                    return res
+                        .status(400)
+                        .json({
+                            error: {
+                                message: `Could not get products for Shop id: ${id}`
+                            }
+                        })
+                }
 
+                return res.json(products);
+            })
+            .catch(err => {
+                next(err);
+            })
         
     })
 
 
+// Get Shops by serivce name
 ShopRouter
     .route(`/search/:name`)
     .get((req, res, next) => {
@@ -82,6 +99,31 @@ ShopRouter
                         .json({
                             error: {
                                 message: `Could not find any shops with name:${name}`
+                            }
+                        })
+                }
+
+                return res.json(shops);
+            })
+            .catch(err => {
+                next(err);
+            })
+    })
+
+ShopRouter
+    .route('/service-type/:service_type')
+    .get((req, res, next) => {
+        const {service_type} = req.params;
+        const db = req.app.get('db');
+
+        return ShopService.getShopByServiceType(db, service_type.toLowerCase())
+            .then((shops) => {
+                if(!shops){
+                    return res
+                        .status(400)
+                        .json({
+                            error: {
+                                message: `Could not find any shops with service type:${service_type}`
                             }
                         })
                 }
