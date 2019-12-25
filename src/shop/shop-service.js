16ce5b8376b0d49b27d74474505b2584b287795a
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-12-25 11:31:53 -0500

    created service to get all products for a given shop

--- src/shop/shop-service.js
+++ src/shop/shop-service.js
@@ -10,13 +10,33 @@ const ShopService = {
     },
     getProductsByShopId(db, id){
         return db
-            .from('shop_products as shprd')
+            .from('products as prd')
+            .select(
+                'prd.id',
+                'prd.item',
+                'prd.price',
+                'prd.description',
+                'prd.image_url',
+            )
+            .leftJoin(
+                'shop_products as shprd',
+                'shprd.product_id',
+                'prd.id'
+            )
+            .where(
+                'shprd.shop_id',
+                id
+            )
     },
     getShopsByName(db, shop_name){
-        console.log(db('shop').where('shop_name', 'like', `*%${shop_name}%*`).toString());
         return db
             .from('shop')
             .where('shop_name', 'like', `%${shop_name}%`);
+    },
+    getShopByServiceType(db, service_type){
+        return db
+            .from('shop')
+            .where({service_type});
     }
 }
 
