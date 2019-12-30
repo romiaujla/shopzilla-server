e0e76a2388ee8e8ccfe711ae487d68530f51f24f
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-12-30 11:20:46 -0500

    added functions to return test data for tables

--- test/test-helpers.js
+++ test/test-helpers.js
@@ -1,5 +1,56 @@
+function makeUsersArray(){
+    return [
+        {"username": "shopuser01", "password" : "password", "avatar_url": "no_url", "user_type": "shop"},
+        {"username": "shopuser02", "password" : "password", "avatar_url": "no_url", "user_type": "shop"},
+        {"username": "shopuser03", "password" : "password", "avatar_url": "no_url", "user_type": "shop"},
+        {"username": "buyer01", "password" : "password", "avatar_url": "no_url", "user_type": "buyer"},
+        {"username": "buyer02", "password" : "password", "avatar_url": "no_url", "user_type": "buyer"}
+    ]
+}
+
+function makeShopProductArray(){
+    return [
+        {}
+    ]
+}
+
 function makeShopsArray(){
     return [
-        
+        {
+            "login_id": 1,
+            "shop_name": "Test Shop 1",
+            "address": "1234 Test Street, Test, TT 12345",
+            "description": "Test description for shop # 1",
+            "service_type" : "food and drinks",
+        },
+        {
+            "login_id": 2,
+            "shop_name": "Test Shop 1",
+            "address": "1234 Test Street, Test, TT 12345",
+            "description": "Test description for shop # 1",
+            "service_type" : "food and drinks",
+        },
+        {
+            "login_id": 3,
+            "shop_name": "Test Shop 1",
+            "address": "1234 Test Street, Test, TT 12345",
+            "description": "Test description for shop # 1",
+            "service_type" : "food and drinks",
+        },
     ]
+}
+
+function makeProductsArray(){
+    return [
+        {"item" : "Product 1", "price" : "10.00", "description": "Test Description for Product 1", "image_url": "imageproduct1.png"},
+        {"item" : "Product 2", "price" : "20.00", "description": "Test Description for Product 2", "image_url": "imageproduct2.png"},
+        {"item" : "Product 3", "price" : "30.00", "description": "Test Description for Product 3", "image_url": "imageproduct3.png"},
+        {"item" : "Product 4", "price" : "40.00", "description": "Test Description for Product 4", "image_url": "imageproduct4.png"}
+    ]
+}
+
+module.exports = {
+    makeShopsArray,
+    makeProductsArray,
+    makeUsersArray,
 }
