6907a170b529c798767b6be241d0a0e5c6de0b24
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-12-30 11:20:19 -0500

    added node environment and require dotenv for tests

--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,7 @@
 const {expect} = require('chai');
 const request = require('supertest');
+process.env.NODE_ENV = 'test';
+require('dotenv').config();
 
 global.expect = expect;
 global.request = request;
