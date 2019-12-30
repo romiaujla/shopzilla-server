77f0ec742f96ef2a8cee8ef8d80b9a210777f308
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-12-30 11:20:00 -0500

    added a products test endpoint and added a test to add the product

---
+++ test/products-endpoint.test.js
@@ -0,0 +1,71 @@
+const knex = require('knex');
+const app = require('../src/app');
+const {
+    makeProductsArray, 
+    makeShopsArray,
+    makeUsersArray,
+} = require('./test-helpers');
+
+describe(`Product Endpoint`, () => {
+    let db;
+    let products = makeProductsArray();
+    let shops = makeShopsArray();
+    let users = makeUsersArray();
+
+    const trucateTables = 'TRUNCATE TABLE shop_products, products RESTART IDENTITY';
+
+    before('make knex instance', () => {
+        db = knex({
+            client: 'pg',
+            connection: process.env.TEST_DATABASE_URL
+        })
+        app.set('db', db);
+    });
+
+    before('cleanup', () => {
+        return db.raw(trucateTables);
+    })
+
+    afterEach('cleanup', () => {
+        return db.raw(trucateTables);
+    })
+
+    after('disconnect from the database', () => {
+        return db.destroy();
+    })
+
+    describe(`POST /api/products`, () => { 
+
+        beforeEach('insert users', () => {
+            return db('users').insert(users);
+        });
+
+        beforeEach('insert shops', () => {
+            return db('shop').insert(shops);
+        });
+
+        it('should create and return a new product when provided with valid data', () => {
+            const newProduct = {
+                "shop_id": "1",
+                "item" : "Product 1", 
+                "price" : "10.00", 
+                "description": "Test Description for Product 1", 
+                "image_url": "imageproduct1.png",
+            }
+
+            return request(app)
+                .post('/api/products')
+                .send(newProduct)
+                .expect(201)
+                .expect(res => {
+                    expect(res.body).to.be.a('object');
+                    expect(res.body).to.include.keys('id', 'item', 'description', 'image_url');
+                    expect(res.body.item).to.equal(newProduct.item);
+                    expect(res.body.price).to.equal(newProduct.price);
+                    expect(res.body.description).to.equal(newProduct.description);
+                    expect(res.body.image_url).to.equal(newProduct.image_url);
+                })
+        })
+
+    })
+})
