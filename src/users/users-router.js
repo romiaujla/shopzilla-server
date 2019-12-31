ca128b02ee60432151bb925a1e2b072799fa6188
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-12-31 12:56:43 -0500

    added functionality to add user with a shop

--- src/users/users-router.js
+++ src/users/users-router.js
@@ -24,6 +24,32 @@ UserRouter
                 name: req.body.name,
             }
         }
+        if(user_type === 'shop'){
+
+            const {
+                shop_name,
+                address,
+                description,
+                start_date,
+                end_date,
+                opening_time,
+                closing_time,
+                service_type,
+                image_url,
+            } = req.body
+
+            shop = {
+                shop_name,
+                address,
+                description,
+                start_date,
+                end_date,
+                opening_time,
+                closing_time,
+                service_type,
+                image_url,
+            }
+        }
 
         const newUser = {
             user_type,
@@ -41,7 +67,7 @@ UserRouter
                 return UserService.insertUser(db, newUserWithHashedPassword)
                     .then(addedUser => {
 
-                        // Adding a buyer id the user type is buyer
+                        // Adding a buyer when the user type is buyer
                         if(addedUser.user_type === 'buyer'){
                             buyer = {
                                 ...buyer,
@@ -52,6 +78,26 @@ UserRouter
                                     return res.status(201).json({
                                         ...addedUser,
                                         ...addedBuyer,
+                                        id: addedUser.id,
+                                        buyer_id: addedBuyer.id,
+                                    })
+                                })
+                                .catch(next);
+                        }
+
+                        // Adding a shop, when the user type is a shop
+                        if(addedUser.user_type === 'shop'){
+                            shop = {
+                                ...shop,
+                                login_id: addedUser.id
+                            }
+                            return UserService.insertShop(db, shop)
+                                .then(addedShop => {
+                                    return res.status(201).json({
+                                        ...addedUser,
+                                        ...addedShop,
+                                        id: addedUser.id,
+                                        shop_id: addedShop.id,
                                     })
                                 })
                                 .catch(next);
