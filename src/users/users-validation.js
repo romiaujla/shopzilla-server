0a9e6435f783b2cf12e51a1ddb95b6682d86a883
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-12-30 21:21:33 -0500

    created validation file for users

---
+++ src/users/users-validation.js
@@ -0,0 +1,42 @@
+function validation(req, res, next){
+    for(const field of ['username', 'password', 'user_type']){
+        if(!req.body[field]){
+            return res.status(400)
+                .json({
+                    error: {
+                        message: `Missing '${field}' in request body`
+                    }
+                })
+        }
+    }
+
+    if(req.body.username.length < 6){
+        return res.status(400).json({
+            error: {
+                message: 'Username must be longer than 6 characters'
+            }
+        })
+    }
+
+    if(req.body.password.length < 6){
+        return res.status(400).json({
+            error: {
+                message: `Password must be longer than 6 characters`
+            }
+        })
+    }
+
+    if(!['shop', 'buyer'].includes(req.body.user_type)){
+        return res.status(400).json({
+            error: {
+                message: 'Invalid user type, must be either shop or buyer'
+            }
+        })
+    }
+
+    next();
+}
+
+module.exports = {
+    validation,
+}
