055a402cd58a25657643e2ae5017bfa92a79f9b5
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2019-12-30 21:35:06 -0500

    get and delete

--- src/products/products-router.js
+++ src/products/products-router.js
@@ -1,64 +1,66 @@
-const express = require("express");
+const express = require('express');
 const ProductRouter = express.Router();
 const jsonParser = express.json();
-const { jwtAuth } = require("../middleware/jwtAuth");
-const { validation } = require("./products-validation");
+const { jwtAuth } = require('../middleware/jwtAuth');
+const { validation } = require('./products-validation');
 const ProductService = require('./products-service');
 
-ProductRouter
-    .route("/")
-    .post(jwtAuth, jsonParser, validation, (req, res, next) => {
-        const db = req.app.get('db');
-        const {
-            shop_id,
-            item,
-            price = 0.00,
-            description = '',
-            image_url = 'product.png'
-        } = req.body
+ProductRouter.route('/')
+  .post(jwtAuth, jsonParser, validation, (req, res, next) => {
+    const db = req.app.get('db');
+    const {
+      shop_id,
+      item,
+      price = 0.0,
+      description = '',
+      image_url = 'product.png'
+    } = req.body;
 
-        const newProduct = {
-            item,
-            price,
-            description,
-            image_url
+    const newProduct = {
+      item,
+      price,
+      description,
+      image_url
+    };
+
+    return ProductService.insertProduct(db, newProduct)
+      .then(addedproduct => {
+        if (!addedproduct) {
+          return res.status(400).json({
+            error: {
+              message: 'Could not add the new product'
+            }
+          });
         }
 
-        return ProductService.insertProduct(db, newProduct)
-            .then((addedproduct) => {
-                if(!addedproduct){
-                    return res
-                        .status(400)
-                        .json({
-                            error: {
-                                message: 'Could not add the new product'
-                            }
-                        })
+        const newShopProduct = {
+          shop_id,
+          product_id: addedproduct.id
+        };
+
+        return ProductService.insertShopProduct(db, newShopProduct)
+          .then(newShopProduct => {
+            if (!newShopProduct) {
+              return res.status(400).json({
+                error: {
+                  message: `Could not add the shop product relation`
                 }
+              });
+            }
 
-                const newShopProduct = {
-                    shop_id,
-                    product_id: addedproduct.id
-                }
+            return res.status(201).json(addedproduct);
+          })
+          .catch(next);
+      })
+      .catch(next);
+  })
+  .get((req, res, next) => {
+    res.send('ok');
+  });
 
-                return ProductService.insertShopProduct(db, newShopProduct)
-                    .then((newShopProduct) => {
-                        if(!newShopProduct){
-                            return res
-                                .status(400)
-                                .json({
-                                    error: {
-                                        message: `Could not add the shop product relation`
-                                    }
-                                })
-                        }
-
-                        return res.status(201).json(addedproduct);
-                    })
-                    .catch(next);
-            })
-            .catch(next);
-    });
-
+ProductRouter.route('/:id').delete((req, res, next) => {
+  const { id } = req.params;
+  res.send(id);
+});
 
 module.exports = ProductRouter;
--- src/products/products-service.js
+++ src/products/products-service.js
@@ -1,19 +1,22 @@
 const ProductService = {
-    insertProduct(db, newProduct){
-        return db('products')
-            .insert(newProduct)
-            .returning('*')
-            .then((rows) => rows[0]);
-    },
-    insertShopProduct(db, newShopProduct){
-        return db('shop_products')
-            .insert(newShopProduct)
-            .returning('*')
-            .then((rows) => rows[0]);
-    },
-    deleteShopProducts(db, id){
-        // return db('')
-    }
-}
+  insertProduct(db, newProduct) {
+    return db('products')
+      .insert(newProduct)
+      .returning('*')
+      .then(rows => rows[0]);
+  },
+  insertShopProduct(db, newShopProduct) {
+    return db('shop_products')
+      .insert(newShopProduct)
+      .returning('*')
+      .then(rows => rows[0]);
+  },
+  deleteShopProducts(db, id) {
+    // return db('')
+  },
+  getProducts(db) {
+    return db('products');
+  }
+};
 
 module.exports = ProductService;
