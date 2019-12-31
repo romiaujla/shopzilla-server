fc1751b6e9724ba9dc6f739d3968c02dfb2222f2
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-12-31 10:25:18 -0500

    created hashpassword and service to insert buyer in db

--- src/users/users-service.js
+++ src/users/users-service.js
@@ -1,8 +1,19 @@
+const bcrypt = require('bcryptjs');
+
 const UserService = {
     insertUser(db, newUser){
         return db('users')
             .insert(newUser)
             .returning('*')
+            .then(rows => rows[0]);
+    },
+    hashPassword(password){
+        return bcrypt.hash(password, 12);
+    },
+    insertBuyer(db, newBuyer){
+        return db('buyer')
+            .insert(newBuyer)
+            .returning("*")
             .then(rows => rows[0]);
     }
 }
