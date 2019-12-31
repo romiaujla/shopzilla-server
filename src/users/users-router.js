2846873211aa90d1ae22a5d54d12a0286f90346e
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-12-31 10:25:04 -0500

    created route to add buyer

--- src/users/users-router.js
+++ src/users/users-router.js
@@ -16,15 +16,46 @@ UserRouter
             user_type,
         } = req.body;
 
+        let buyer = {};
+        let shop = {};
+
+        if(user_type === 'buyer'){
+            buyer = {
+                name: req.body.name,
+            }
+        }
+
         const newUser = {
             user_type,
             username,
-            password: bcrypt.hash(password, 12),
+            password,
         }
         
-        return UserService.insertUser(db, newUser)
-            .then((addedUser) => {
-                res.status(201).json(addedUser);
+        return UserService.hashPassword(password)
+            .then((hashedPassword) => {
+                const newUserWithHashedPassword = {
+                    ...newUser,
+                    password: hashedPassword
+                }
+
+                return UserService.insertUser(db, newUserWithHashedPassword)
+                    .then(addedUser => {
+                        if(addedUser.user_type === 'buyer'){
+                            buyer = {
+                                ...buyer,
+                                login_id: addedUser.id
+                            }
+                            return UserService.insertBuyer(db, buyer)
+                                .then(addedBuyer => {
+                                    return res.status(201).json({
+                                        ...addedUser,
+                                        ...addedBuyer,
+                                    })
+                                })
+                                .catch(next);
+                        }
+                    })
+                    .catch(next);
             })
             .catch(next);
     })
