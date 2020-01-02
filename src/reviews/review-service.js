274857aa551849938b7baf9b87b153c2ff0819cb
Raman Aujla <ramanpreet.aujla@gmail.com>
2020-01-02 17:53:18 -0500

    implemented the service to insert a review in the db

--- src/reviews/review-service.js
+++ src/reviews/review-service.js
@@ -2,7 +2,13 @@ const ReviewService = {
     getReviewsByShopId(db, shop_id){
         return db('reviews')
             .where({shop_id});
-    },    
+    },
+    insertReview(db, newReview){
+        return db('review')
+            .insert(newReview)
+            .returning('*')
+            .then((rows) => row[0]);
+    }    
 }
 
 module.exports = ReviewService;
