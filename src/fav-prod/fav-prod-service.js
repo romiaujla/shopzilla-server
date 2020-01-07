c2f56aa914cb71d1305e6fd64801e1ea2fa70314
Raman Aujla <ramanpreet.aujla@gmail.com>
2020-01-07 10:49:55 -0500

    added a service to return an array of favourite products

--- src/fav-prod/fav-prod-service.js
+++ src/fav-prod/fav-prod-service.js
@@ -1,3 +1,8 @@
 const FavouriteService = {
-    
+    getFavourites(db, buyer_id){
+        return db('favourite_products')
+            .where({buyer_id});
+    }
 }
+
+module.exports = FavouriteService;
