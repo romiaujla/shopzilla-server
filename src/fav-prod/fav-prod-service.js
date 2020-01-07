fef34aa611187cfb065ae3edb441347399976664
Raman Aujla <ramanpreet.aujla@gmail.com>
2020-01-07 11:32:52 -0500

    created service to add favourite products

--- src/fav-prod/fav-prod-service.js
+++ src/fav-prod/fav-prod-service.js
@@ -2,6 +2,12 @@ const FavouriteService = {
     getFavourites(db, buyer_id){
         return db('favourite_products')
             .where({buyer_id});
+    },
+    insertFavourite(db, newFav){
+        return db('favourite_products')
+            .insert(newFav)
+            .returning('*')
+            .then(rows => rows[0]);
     }
 }
 
