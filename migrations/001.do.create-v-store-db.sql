4137c321f413d78853bca9a6162a53d198b614f7
Raman Aujla <ramanpreet.aujla@gmail.com>
2020-01-07 21:10:16 -0500

    fixed on delete cascade

--- migrations/001.do.create-v-store-db.sql
+++ migrations/001.do.create-v-store-db.sql
@@ -87,8 +87,8 @@ CREATE TABLE reviews(
 );
 
 CREATE TABLE favourite_products(
-    product_id INTEGER REFERENCES products(id) NOT NULL ON DELETE CASCADE,
-    buyer_id INTEGER REFERENCES buyer(id) NOT NULL ON DELETE CASCADE,
+    product_id INTEGER REFERENCES products(id) ON DELETE CASCADE,
+    buyer_id INTEGER REFERENCES buyer(id) ON DELETE CASCADE,
     PRIMARY KEY (product_id, buyer_id)
 );
 
